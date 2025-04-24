bed698af6220f2f775cc4074d2c75ebca29ea592
yuval <yuval.solaz@gmail.com>
2025-04-24 18:08:23 +0300

    start

---
+++ config.py
@@ -0,0 +1,13 @@
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Access OpenAI API key
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+LLM_MODEL = os.getenv("LLM_MODEL", "gpt-4o-mini")
+LLM_TEMPERATURE = os.getenv("LLM_TEMPERATURE", "0.0")
+
+
